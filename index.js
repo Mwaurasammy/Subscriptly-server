e76a209aff13bc3055c0c06c4f56e0af7ca04802
SammyMwaura <mwaurasammy500@gmail.com>
2024-08-16 10:14:52 +0300

    server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 5000
+const jsonServer = require('json-server');
+const cors = require('cors');  // Import the CORS package
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
 
-server.use(middlewares)
-server.use(router)
+server.use(cors()); // Enable CORS for all routes
+server.use(middlewares);
+server.use(router);
+
 server.listen(port, () => {
-  console.log('JSON Server is running on port', port)
-})
+  console.log('JSON Server is running on port', port);
+});
